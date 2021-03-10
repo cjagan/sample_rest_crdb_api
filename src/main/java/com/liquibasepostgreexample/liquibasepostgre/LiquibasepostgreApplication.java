72b615706206d675a8bdd385d21773fde773858f
Datla <2010480@cognizant.com>
2021-03-10 18:59:45 +0530

    Initial Commit

---
+++ src/main/java/com/liquibasepostgreexample/liquibasepostgre/LiquibasepostgreApplication.java
@@ -0,0 +1,13 @@
+package com.liquibasepostgreexample.liquibasepostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibasepostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibasepostgreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liquibasepostgreexample/liquibasepostgre/LiquibasepostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquibasepostgreexample.liquibasepostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibasepostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
